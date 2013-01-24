a695a8f0899a0c8baa8da5d5e1b7faecff7f3a3e
Elisha Cook <elisha@elishacook.com>
2013-01-24 16:54:15 -0500

    Fixed setup.py one more time

--- setup.py
+++ setup.py
@@ -1,16 +1,12 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(
     name = 'wikid',
     version = '0.0.2',
     packages = ['wikid'],
-    data_files = [
-    	('wikid/data/css', ['wikid/data/css/styles.css']),
-    	('wikid/data/js', ['wikid/data/js/jquery.js']),
-    	('wikid/data/js', ['wikid/data/js/search.js'])
-    ],
+    package_data = { 'wikid': ['data/js/*', 'data/css/*'] },
     scripts = ['scripts/wikid'],
-    install_requires = ['markdown'],
+    requires = ['markdown'],
     description = 'A stupid-simple, file-based wiki.',
     author = 'Elisha Cook',
     author_email = 'elisha@elishacook.com'
