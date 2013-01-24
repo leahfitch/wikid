035063e4e5a85024cb574b250e16c4694331a4df
Elisha Cook <elisha@elishacook.com>
2013-01-24 15:52:30 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@ from setuptools import setup
 setup(
     name = 'wikid',
     version = '0.0.1',
-    py_modules = ['wikid'],
+    packages = ['wikid'],
+    package_data = { 'wikid': ['data/js/*', 'data/css/*'] },
+    scripts = ['scripts/wikid'],
     install_requires = ['markdown'],
     description = 'A stupid-simple, file-based wiki.',
     author = 'Elisha Cook',
