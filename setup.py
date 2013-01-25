80237fb7d0b34fa5a45ac3c19223c1e943a55fab
Elisha Cook <elisha@elishacook.com>
2013-01-25 10:56:28 -0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -2,12 +2,23 @@ from distutils.core import setup
 
 setup(
     name = 'wikid',
-    version = '0.0.3',
+    version = '0.0.4',
     packages = ['wikid'],
     package_data = { 'wikid': ['data/js/*', 'data/css/*'] },
     scripts = ['scripts/wikid'],
     requires = ['markdown'],
     description = 'A stupid-simple, file-based wiki.',
     author = 'Elisha Cook',
-    author_email = 'elisha@elishacook.com'
+    author_email = 'elisha@elishacook.com',
+    url = 'https://github.com/elishacook/wikid',
+    download_url = 'https://github.com/elishacook/wikid/tarball/master',
+    license = 'MIT',
+    classifiers = [
+    	'Development Status :: 4 - Beta',
+    	'Intended Audience :: Developers',
+    	'Environment :: Console',
+	    'Environment :: Web Environment',
+    	'License :: OSI Approved :: MIT License',
+    	'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ]
 )
