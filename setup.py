fcd4f902238521cefe98d762482bcd8883b0ed1a
Elisha Cook <elisha@elishacook.com>
2013-07-08 20:46:01 -0400

    Updated version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'wikid',
-    version = '0.0.7',
+    version = '0.0.8',
     packages = ['wikid'],
     package_data = { 'wikid': ['data/js/*', 'data/css/*'] },
     scripts = ['scripts/wikid'],
