c26bbc4f6340228620f590c371b83b1b8d50a830
Elisha Cook <elisha@elishacook.com>
2013-07-29 11:10:03 -0400

    Changed download url to github project

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name = 'wikid',
-    version = '0.0.9',
+    version = '0.0.10',
     packages = ['wikid'],
     package_data = { 'wikid': ['data/js/*', 'data/css/*'] },
     scripts = ['scripts/wikid'],
@@ -11,7 +11,7 @@ setup(
     author = 'Elisha Cook',
     author_email = 'elisha@elishacook.com',
     url = 'https://github.com/elishacook/wikid',
-    download_url = 'https://github.com/elishacook/wikid/tarball/master',
+    download_url = 'https://github.com/elishacook/wikid',
     license = 'MIT',
     classifiers = [
     	'Development Status :: 4 - Beta',
